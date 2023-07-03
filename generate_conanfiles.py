4ef2c0d9c65fdeb210adf8a33b2b29bab01dfa83
Tanner Bitz <tannerbitz@gmail.com>
2023-07-03 17:20:40 -0400

    conanfile gen skeleton

--- generate_conanfiles.py
+++ generate_conanfiles.py
@@ -4,6 +4,18 @@ import yaml
 import os
 import glob
 from pathlib import Path
+from jinja2 import Environment, FileSystemLoader
+
+
+def render():
+    env = Environment(
+        loader=FileSystemLoader("templates")
+    )
+    template = env.get_template("cmake_conanfile.jinja")
+
+    args = {'name': "Tanner Bitz", 'confidence': 'low' }
+    print(template.render(**args))
+
 
 def read_repos(repos_file: str) -> dict:
     is_file = os.path.isfile(repos_file)
@@ -14,21 +26,28 @@ def read_repos(repos_file: str) -> dict:
         repos = yaml.safe_load(file)
         return repos
 
+
 def get_package_xml_files(root_path):
     return glob.glob("**/package.xml", root_dir=root_path, recursive=True)
+
 
 def has_file(dirname, fname):
     return os.path.isfile(os.path.join(dirname, fname))
 
+
 def has_cmakelists(dirname):
     return has_file(dirname, "CMakeLists.txt")
+
 
 def has_setup_py(dirname):
     return has_file(dirname, "setup.py")
 
-if __name__ == "__main__":
-    repos = read_repos("ros2.repos")
 
-    src_dir = Path(__file__).parent.absolute() / "src"
-    package_xmls = get_package_xml_files(src_dir)
-    print(package_xmls)
+if __name__ == "__main__":
+    # repos = read_repos("ros2.repos")
+
+    # src_dir = Path(__file__).parent.absolute() / "src"
+    # package_xmls = get_package_xml_files(src_dir)
+    # print(package_xmls)
+
+    render()
